5ca4f0246dc67c5089d7517836784fb0a1529a51
carlos eduardo damian perez <dapecarlos@gmail.com>
2023-04-21 11:18:30 -0600

    feat(router): added routing and controllers

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import router from "./router";
+dotenv.config();
 
 const app = express();
 
@@ -25,7 +28,10 @@ server.listen(8080, () => {
 });
 
 const MONGO_URL = process.env.MONGO_URL;
+console.log(MONGO_URL);
 
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(MONGO_URL.toString());
 mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router);
